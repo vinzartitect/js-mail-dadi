ccbc47d84ff94ead51a97592b85307764d43b605
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-15 17:38:39 +0100

    correzione esercizio mail

--- mail/asset/js/main.js
+++ mail/asset/js/main.js
@@ -4,18 +4,33 @@
 // stampa un messaggio appropriato sull’esito del controllo.
 // (non usare INCLUDES)
 
-let iscritti = [ "pippobaudo@gmail.com", "pippofranco@libero.it", "pippoinzaghi@hotmail.it", ];
+let iscritti = [ 
+    `pippobaudo@gmail.com`, 
+    `pippofranco@libero.it`, 
+    `pippoinzaghi@hotmail.it`, 
+    `pippopotamo@gmail.com`,
+    `pippotassio@hotmail.it`,
+    `pippolonio@libero.it`,
+    `pippotenza@gmail.com`,
+];
 
 let dammiMail = prompt("inserisci la tua mail");
 console.log("la mail inserita è " + dammiMail);
 
-for ( i=0; i < iscritti.length; i++ ) {
+let mailTrovata = false;
 
-    if ( dammiMail === iscritti[i] ) {
-        console.log("risulti gia registrato!");
-        document.getElementById("risposta_out").innerHTML = `risulti gia registrato!`;
-    } else {
-        console.log("che aspetti! corri a registrarti!");
-        document.getElementById("risposta_out").innerHTML = `che aspetti! corri a registrarti!`;
+for ( let i=0; i < iscritti.length; i++ ) {
+
+    if ( dammiMail == iscritti[i] ) {
+        mailTrovata = true;
     }
+
+}
+
+if ( mailTrovata == true ) {
+    console.log("risulti gia registrato!");
+    document.getElementById("risposta_out").innerHTML = `risulti gia registrato!`;
+} else {
+    console.log("che aspetti! corri a registrarti!");
+    document.getElementById("risposta_out").innerHTML = `che aspetti! corri a registrarti!`;
 }
